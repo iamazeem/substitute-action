7809dc987b157f5b3a95dbcf0f7857d97e2cbaaa
Azeem Sajid <azeem.sajid@gmail.com>
2023-03-19 19:11:46 +0500

    Update script.
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>

--- substitute.bash
+++ substitute.bash
@@ -1,56 +1,71 @@
 #!/bin/bash
 
-echo "::group::Validating envsubst command"
+validate_envsubst() {
+  echo "::group::Validating envsubst command"
 
-if ! which envsubst; then
-  echo "envsubst command not found!"
-  echo "::error::envsubst command not found!"
-  exit 1
-fi
+  if ! which envsubst; then
+    echo "envsubst command not found!"
+    echo "::error::envsubst command not found!"
+    exit 1
+  fi
 
-echo "::debug::Printing envsubst version"
-envsubst --version
+  echo "::debug::Printing envsubst version"
+  envsubst --version
 
-echo "::endgroup::"
+  echo "::endgroup::"
+}
 
-echo "::group::Validating inputs"
+validate_inputs() {
+  echo "::group::Validating inputs"
 
-if [[ -n $ENV_FILES ]]; then
-  echo "Validating and listing env files"
+  if [[ -n $ENV_FILES ]]; then
+    echo "Validating and listing env files"
+    for FILE in $ENV_FILES; do
+      if [[ ! -f $FILE ]]; then
+        echo "$FILE does not exist!"
+        exit 1
+      else
+        echo "- $FILE"
+      fi
+    done
+  fi
+
+  if [[ -n $INPUT_FILES ]]; then
+    echo "Validating and listing input files"
+    for FILE in $INPUT_FILES; do
+      if [[ ! -f $FILE ]]; then
+        echo "$FILE does not exist!"
+        exit 1
+      else
+        echo "- $FILE"
+      fi
+    done
+  fi
+
+  echo "In-place substitution? [$IN_PLACE]"
+
+  echo "::endgroup::"
+}
+
+substitute() {
+  echo "::group::Substituting"
+
+  echo "Sourcing environment variables from env files"
+  set -a
   for FILE in $ENV_FILES; do
-    if [[ ! -f $FILE ]]; then
-      echo "$FILE does not exist!"
-      exit 1
-    else
-      echo "- $FILE"
-    fi
+    source "$FILE"
   done
-fi
+  set +z
 
-if [[ -n $INPUT_FILES ]]; then
-  echo "Validating and listing input files"
+  echo "Substituting enviornment variables in input files"
   for FILE in $INPUT_FILES; do
-    if [[ ! -f $FILE ]]; then
-      echo "$FILE does not exist!"
-      exit 1
-    else
-      echo "- $FILE"
-    fi
+    echo "Substituting [$FILE]"
+    envsubst < "$FILE"
   done
-fi
 
-if [[ $IN_PLACE == true ]]; then
-  echo "In-place substitution is enabled!"
-else
-  echo "In-place substitution is disabled!"
-fi
+  echo "::endgroup::"
+}
 
-echo "::endgroup::"
-
-echo "::group::Substituting"
-
-# set -a
-# source file.env
-# set +a
-
-echo "::endgroup::"
+validate_envsubst
+validate_inputs
+substitute
