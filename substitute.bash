4281cc7a0bce9ee1bd06e3dc723a2d02d0806b15
Azeem Sajid <azeem.sajid@gmail.com>
2023-03-19 19:01:22 +0500

    Update script.
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>

--- substitute.bash
+++ substitute.bash
@@ -16,30 +16,34 @@ echo "::endgroup::"
 echo "::group::Validating inputs"
 
 if [[ -n $ENV_FILES ]]; then
-  echo "Listing env files"
+  echo "Validating and listing env files"
   for FILE in $ENV_FILES; do
     if [[ ! -f $FILE ]]; then
       echo "$FILE does not exist!"
       exit 1
     else
-      echo "::debug::$FILE exists!"
+      echo "- $FILE"
     fi
   done
 fi
 
 if [[ -n $INPUT_FILES ]]; then
-  echo "Listing input files"
+  echo "Validating and listing input files"
   for FILE in $INPUT_FILES; do
     if [[ ! -f $FILE ]]; then
       echo "$FILE does not exist!"
       exit 1
     else
-      echo "::debug::$FILE exists!"
+      echo "- $FILE"
     fi
   done
 fi
 
-echo "In-place substitution? [$IN_PLACE]"
+if [[ $IN_PLACE == true ]]; then
+  echo "In-place substitution is enabled!"
+else
+  echo "In-place substitution is disabled!"
+fi
 
 echo "::endgroup::"
 
