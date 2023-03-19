53eddb83f3b4b69e349286404d7d4f8c5f0f6dc2
Azeem Sajid <azeem.sajid@gmail.com>
2023-03-19 18:29:24 +0500

    Update script.
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>

--- substitute.bash
+++ substitute.bash
@@ -1,42 +1,43 @@
 #!/bin/bash
 
-echo "Running $0"
-
 echo "::group::Validating envsubst command"
 
 if ! which envsubst; then
-  echo "envsubst command not found"
-  echo "::error:: envsubst command not found"
+  echo "envsubst command not found!"
+  echo "::error::envsubst command not found!"
   exit 1
 fi
 
+echo "::debug::Printing envsubst version"
 envsubst --version
 
 echo "::endgroup::"
 
 echo "::group::Validating inputs"
 
-if [[ -n $ENV_FILES ]]; then
-  echo "Listing env files [$ENV_FILES]"
+if [[ -z $ENV_FILES ]]; then
+  echo "Listing env files"
+  for FILE in $ENV_FILES; do
+    if [[ ! -f $FILE ]]; then
+      echo "$FILE does not exist!"
+      exit 1
+    else
+      echo "::debug::$FILE exists!"
+    fi
+  done
 fi
-
-for FILE in $ENV_FILES; do
-  if [[ ! -f $FILE ]]; then
-    echo "$FILE does not exist!"
-    exit 1
-  fi
-done
 
 if [[ -n $INPUT_FILES ]]; then
-  echo "Listing input files [$INPUT_FILES]"
+  echo "Listing input files"
+  for FILE in $INPUT_FILES; do
+    if [[ ! -f $FILE ]]; then
+      echo "$FILE does not exist!"
+      exit 1
+    else
+      echo "::debug::$FILE exists!"
+    fi
+  done
 fi
-
-for FILE in $INPUT_FILES; do
-  if [[ ! -f $FILE ]]; then
-    echo "$FILE does not exist!"
-    exit 1
-  fi
-done
 
 echo "In-place substitution? [$IN_PLACE]"
 
