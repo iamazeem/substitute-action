20882949973bf0a15403c3ccf587b5b406d0862c
Azeem Sajid <azeem.sajid@gmail.com>
2023-03-19 17:26:00 +0500

    Update script.
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>

--- substitute.bash
+++ substitute.bash
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+echo "::group:: Substitue environment variables"
+
+echo "::debug:: Running $0"
+
 if ! which envsubst; then
-  echo ""
+  echo "::error:: \`envsubst\` not found, exiting"
   exit 1
 fi
 
 envsubst --version
+
+if [[ -n $ENV_FILES ]]; then
+  echo "::notice:: Listing env files [$ENV_FILES]"
+fi
+
+if [[ -n $INPUT_FILES ]]; then
+  echo "::notice:: Listing input files [$INPUT_FILES]"
+fi
+
+echo "::notice:: in-place = $IN_PLACE"
+
+# set -a
+# source file.env
+# set +a
+
+echo "::endgroup:: "
